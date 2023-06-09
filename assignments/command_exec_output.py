f11a8a3231a3e99d46c11cdc9fc3bbef959219b6
Sreejish Nair <sreejish.nair@gslab.com>
2023-06-09 15:38:08 +0530

    email functionality implemented on file name test_remote_resource

---
+++ assignments/command_exec_output.py
@@ -0,0 +1,33 @@
+import subprocess
+
+POWERSHELL_CMD = ['ssh', 'gs-3922@192.168.56.101', '-p', '22']
+SUB_COMMAND = "exit"
+
+
+def validate(func):
+    return_code = func()
+    if return_code == 0:
+        print("Output generated on the file 'resource.txt'")
+
+    else:
+        print("Error: Could not generate the output file")
+
+
+@validate
+def ssh_connection():
+    process = subprocess.Popen(POWERSHELL_CMD,
+                               stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+
+    process.stdin.write(SUB_COMMAND.encode())
+    process.stdin.flush()
+
+    output, error = process.communicate()
+    output = output.decode()
+
+    with open("./output.txt", mode="w") as cmd_output:
+        cmd_output.write(output)
+
+    return_code = process.wait()
+    return return_code
+
+
--- assignments/test_remote_resource.py
+++ assignments/test_remote_resource.py
@@ -1,16 +1,46 @@
 from paramiko.client import SSHClient
 from scp import SCPClient
-import pytest
+import ssl
+import smtplib
+from email.message import EmailMessage
 
 SSH_USER = "gs-3922"
 SSH_PASSWORD = "root"
 SSH_HOST = "192.168.56.101"
 SSH_PORT = 22
 
-client = SSHClient()
 
-client.load_system_host_keys()
-try:
+def send_email(subject, body):
+
+    email_sender = "nairsanju98@gmail.com"
+    email_password = 'qdcdmqplggiriljo'
+    email_receiver = "nairsreejish@gmail.com"
+
+    subject = subject
+    body = body
+
+    em = EmailMessage()
+
+    em['Subject'] = subject
+    em['From'] = email_sender
+    em['To'] = email_receiver
+    em.set_content(body)
+
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+        smtp.login(email_sender, email_password)
+        smtp.sendmail(email_sender, email_receiver, em.as_string())
+
+    s = smtplib.SMTP('localhost')
+    s.sendmail(email_sender, [email_receiver], em.as_string())
+    s.quit()
+
+
+def test_remote_resource():
+    client = SSHClient()
+    client.load_system_host_keys()
+
     client.connect(SSH_HOST, port=SSH_PORT,
                    username=SSH_USER,
                    password=SSH_PASSWORD,
@@ -24,13 +54,29 @@ try:
     comm = "pytest test_usage.py -v"
     client.exec_command("cd /home/gs-3922")
     stdin, stdout, stderr = client.exec_command(comm)
+    res = stdout.read().decode()
+    # print(res)
+
+    resource = res.splitlines()[13]
+    # print("Resource==>", resource)
+
+    cpu = float(resource.split(" ")[2].strip(","))
+    ram = float(resource.split(" ")[5])
+    # print(cpu,ram)
 
     with open("./resource.txt", mode="w") as resource_file:
-        for line in stdout.readlines():
-            resource_file.writelines(line)
+        file = res.splitlines()
+        for line in file:
+            resource_file.writelines(f"{line}\n")
+        print("Output generated on the file 'resource.txt'")
 
-except Exception:
-    print("Failed to establish connection.")
+    if cpu > 20 or ram > 20:
+        subject = "Resource Usage Alert"
+        body = f"Alert: CPU Usage: {cpu}%\nRAM Usage: {ram}%"
+        send_email(subject, body)
 
-finally:
     client.close()
+
+
+test_remote_resource()
+
